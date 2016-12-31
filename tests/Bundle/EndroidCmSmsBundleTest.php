c4ad7665c7dbdbf03d815cc82fa156f446655d14
endroid <info@endroid.nl>
2016-12-31 14:01:18 +0000

    Minor change

--- tests/Bundle/EndroidCmSmsBundleTest.php
+++ tests/Bundle/EndroidCmSmsBundleTest.php
@@ -7,8 +7,6 @@
  * with this source code in the file LICENSE.
  */
 
-use PHPUnit_Framework_TestCase;
-
 class EndroidCmSmsBundleTest extends PHPUnit_Framework_TestCase
 {
     public function testNoTestsYet()
