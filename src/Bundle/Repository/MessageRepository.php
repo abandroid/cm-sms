8f843e310401f9dfa9659a8b6a4cb6e747984cf0
endroid <info@endroid.nl>
2017-03-30 13:38:29 +0000

    Set message ordering

--- src/Bundle/Repository/MessageRepository.php
+++ src/Bundle/Repository/MessageRepository.php
@@ -15,4 +15,16 @@ class MessageRepository extends EntityRepository
         $this->getEntityManager()->merge($message);
         $this->getEntityManager()->flush();
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        if (is_null($orderBy)) {
+            $orderBy = ['dateUpdated' => 'DESC'];
+        }
+
+        return parent::findBy($criteria, $orderBy, $limit, $offset);
+    }
 }
