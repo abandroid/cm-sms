de9d2035fa07788e9819407f7bfe703c03b4c7d1
endroid <info@endroid.nl>
2017-08-18 21:50:32 +0000

    Auto register asset package for manifest

--- src/Bundle/CmSmsBundle/DependencyInjection/EndroidCmSmsExtension.php
+++ src/Bundle/CmSmsBundle/DependencyInjection/EndroidCmSmsExtension.php
@@ -10,12 +10,13 @@
 namespace Endroid\CmSms\Bundle\CmSmsBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
-class EndroidCmSmsExtension extends Extension
+class EndroidCmSmsExtension extends Extension implements PrependExtensionInterface
 {
     /**
      * {@inheritdoc}
@@ -30,5 +31,22 @@ class EndroidCmSmsExtension extends Extension
 
         $clientDefinition = $container->getDefinition('endroid.cm_sms.client');
         $clientDefinition->setArguments([$config['product_token'], $config['defaults'], $config['delivery_phone_numbers'], $config['disable_delivery']]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function prepend(ContainerBuilder $container)
+    {
+        // Add the cmsms package to the assets configuration so the correct manifest is used
+        $container->prependExtensionConfig('framework', [
+            'assets' => [
+                'packages' => [
+                    'endroid_cm_sms' => [
+                        'json_manifest_path' => '%kernel.project_dir%/public/bundles/endroidcmsms/build/manifest.json'
+                    ]
+                ]
+            ]
+        ]);
     }
 }
