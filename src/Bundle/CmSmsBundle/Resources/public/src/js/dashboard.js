f9224e6b41592eb168bc7ec6a63d081902889400
endroid <info@endroid.nl>
2017-10-22 21:30:44 +0000

    Remove bundle

--- src/Bundle/CmSmsBundle/Resources/public/src/js/dashboard.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Application from './components/Application';
-
-ReactDOM.render(
-    <Application loadPath={appConfig.loadPath} testPath={appConfig.testPath} />,
-    document.getElementById('application')
-);
